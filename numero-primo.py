6f7176a73af737764c8f6b41bb6a887cd0d5fd74
Johnes Sombra <jhonesk5@gmail.com>
2023-02-28 16:53:58 -0500

    Aplicativo para trabalhar com números primos

--- numero-primo.py
+++ numero-primo.py
@@ -13,9 +13,20 @@ time.sleep(1)
 
 input('Tecle Enter para continuar')
 
+# Calculador de números primos
+
 def primo(n):
     for val in range(2,n):
         if n % val == 0:
             return False
 
     return True
+        
+def exibe():
+    n = int(input('Exibir primos até o número: '))
+    for val in range(2,n+1):
+        if(primo(val)):
+            print(val)
+    
+while True:
+    exibe()
